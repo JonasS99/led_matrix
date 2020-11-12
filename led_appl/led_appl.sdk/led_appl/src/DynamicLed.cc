2d07028771060e99a9fa819c94299310e66c7525
Bastian Melchior <bastianmelchior@t-online.de>
2020-11-12 11:27:46 +0100

    update rainbow animation - color correction &  colum width adaption

--- led_appl/led_appl.sdk/led_appl/src/DynamicLed.cc
+++ led_appl/led_appl.sdk/led_appl/src/DynamicLed.cc
@@ -48,7 +48,7 @@ void DynamicLed_animation(dynamicLedMode_t animation, bool firstAccess)
 //0,180,0 irgendein gruen
 //180,180,0 irgendein gelb
 //180,0,0 irgendein rot
-u32 colorArray[5][3] = {{139,139,139},{0,0,180},{0,180,0},{180,180,0},{255,0,0}};
+u32 colorArray[5][3] = {{139,0,139},{0,0,180},{0,180,0},{180,180,0},{255,0,0}};
 static void rainbowAnimation(void)
 {
 	static u32 animationCount = 0;
@@ -62,12 +62,12 @@ static void rainbowAnimation(void)
 			for(u8 x=0 ; x<20 ; x++)
 			{
 				LedMatrixDriver_SetLed(x,y,colorArray[colorCount][0],colorArray[colorCount][1],colorArray[colorCount][2]);
-				if(x==4 || x==8 || x==12 || x==16) colorCount++;
+				if(x==3 || x==7 || x==11 || x==15) colorCount++;
 			}
 		}
 	}
 	firstAccessCheck = 0;
-	if(animationCount>=100)
+	if(animationCount>=10)
 	{
 		LedMatrixDriver_SlideAllLed();
 		animationCount = 0;
