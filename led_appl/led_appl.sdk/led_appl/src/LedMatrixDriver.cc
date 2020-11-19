c20950bb0f49eaa64f79e35cc73ff6b0052ed306
Oliver <oliver.leibbrand@outlook.com>
2020-11-19 14:09:34 +0100

    s

--- led_appl/led_appl.sdk/led_appl/src/LedMatrixDriver.cc
+++ led_appl/led_appl.sdk/led_appl/src/LedMatrixDriver.cc
@@ -160,7 +160,7 @@ void LedMatrixDriver_CreateSquare(u8 startX, u8 startY, u8 size)
 	}
 }
 /*
- * change rgb color smoothly.
+ * change rgb color smoothly
  */
 void LEDMatrixDriver_SetLEDs_ForLoop(u16 i1,u16 i2,u16 j1,u16 j2,u16 r,u16 g,u16 b){
 	for(u16 i=i1; i<=i2; i++) for(u16 j=j1; j<=j2; j++) LedMatrixDriver_SetLed(i, j, r, g, b);
