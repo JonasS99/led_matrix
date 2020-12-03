db50ccb0030fa6bd04ce89af0fe44c73dd5c0ba4
Bastian Melchior <bastianmelchior@t-online.de>
2020-12-03 10:49:16 +0100

    switch smooth color change in rainbow animation from horizontal change to vertical color change

--- led_appl/led_appl.sdk/led_appl/src/DynamicLed.cc
+++ led_appl/led_appl.sdk/led_appl/src/DynamicLed.cc
@@ -70,15 +70,16 @@ static void rainbowAnimation(void)
 	if(firstAccessCheck==1)
 	{
 		LedMatrixDriver_ClearAllLed();
-		for(u8 y=0 ; y<20 ; y++)
+		for(u8 x=0 ; x<20 ; x++)
 		{
 			colorCount = 0;
-			for(u8 x=0 ; x<20 ; x++)
+			for(u8 y=0 ; y<20 ; y++)
 			{
 				LedMatrixDriver_SetLed(x,y,r,g,b);//colorArray[colorCount][0],colorArray[colorCount][1],colorArray[colorCount][2]);
-				LedMatrixDriver_ShiftColors(&r,&g,&b);
+
 				//if(x==3 || x==7 || x==11 || x==15) colorCount++;
 			}
+			LedMatrixDriver_ShiftColors(&r,&g,&b);
 		}
 	}
 	firstAccessCheck = 0;
